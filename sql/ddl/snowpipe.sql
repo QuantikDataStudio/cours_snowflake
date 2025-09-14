5d1b87f95a19aeaaec3ae0abeb2cc909803fe665
aalloul <adam@adamdataservices.com>
2025-09-14 13:48:40 +0200

    add pipe definition

--- sql/ddl/snowpipe.sql
+++ sql/ddl/snowpipe.sql
@@ -1,6 +1,23 @@
+-- Création d'un pipe d'ingestion automatique
 CREATE PIPE raw.load_raw_data
   AUTO_INGEST = TRUE
   AS
-  COPY INTO raw.raw_events
-  FROM @raw.internal_stage
-  FILE_FORMAT = (TYPE = 'CSV');
+    COPY INTO RAW.RAW_EVENTS (event_timestamp, process_name, process_id, message)
+    FROM
+    (SELECT
+        $1 AS event_timestamp,
+        $2 AS process_name,
+        $3 AS process_id,
+        $4 AS message
+    FROM @raw.internal_stage)
+  FILE_FORMAT = raw.csv_file;
+
+-- Monitoring du chargement des données par Snowpipe
+select *
+from table(
+    information_schema.copy_history(
+        TABLE_NAME=>'raw_events',
+        START_TIME=> DATEADD(hours, -1, CURRENT_TIMESTAMP()
+        )
+    )
+);
